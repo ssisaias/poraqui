363388dae7ec696ede98ed38b9cd98f611011d61
ssisaias <luiz.isaias95@gmail.com>
2016-11-29 16:34:04 -0300

    Commit inicial

---
+++ src/main/java/com/qxd/poraqui/PoraquiApplication.java
@@ -0,0 +1,12 @@
+package com.qxd.poraqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoraquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoraquiApplication.class, args);
+	}
+}
